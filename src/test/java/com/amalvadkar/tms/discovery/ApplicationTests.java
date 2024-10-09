8e5b54ed62b0433c8d5851a2b0db73b547d2c95e
Abhishek Malvadkar <abhi@dev.com>
2024-10-09 18:58:36 +0530

    GH-5 : Created discovery service

---
+++ src/test/java/com/amalvadkar/tms/discovery/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.tms.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void should_load_context() {
+	}
+
+}
